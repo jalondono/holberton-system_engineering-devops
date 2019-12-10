6e9bf38068a9cd21d0c2a45d024c3808543ddc8b
jalondono <juanlondono151776@hotmail.com>
2019-12-10 13:43:58 -0500

    upload task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,7 +1,11 @@
 # added lines to ssh confif
-file_line { 'addLines':
+file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => 'etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
+}
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton'
 }
