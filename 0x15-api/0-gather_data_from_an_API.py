33bc80242bfc39316debb2d1477adeff863cca0e
jalondono <juanlondono151776@hotmail.com>
2020-02-03 17:54:27 -0500

    Deleted validation len args

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,26 +7,25 @@ from sys import argv
 import json
 
 if __name__ == '__main__':
-    if len(argv) == 2:
-        user_id = argv[1]
+    user_id = argv[1]
 
-        tasks_url = 'https://jsonplaceholder.typicode.com/todos'\
-                    + '?userId=' + user_id
-        user_url = 'https://jsonplaceholder.typicode.com/users'\
-                   + '?id=' + user_id
-        tasks_req = requests.get(tasks_url)
-        user_req = requests.get(user_url)
-        data_task = json.loads(tasks_req.text)
-        data_user = json.loads(user_req.text)
-        user = data_user[0]['name']
-        number_of_task = len(data_task)
-        done_task = 0
-        completed_task = []
-        for task in data_task:
-            if task.get('completed'):
-                done_task += 1
-                completed_task.append(task.get('title'))
-        print("Employee {} is done with tasks({}/{}):".
-              format(user, done_task, number_of_task))
-        for title in completed_task:
-            print("\t {}".format(title))
+    tasks_url = 'https://jsonplaceholder.typicode.com/todos' \
+                + '?userId=' + user_id
+    user_url = 'https://jsonplaceholder.typicode.com/users' \
+               + '?id=' + user_id
+    tasks_req = requests.get(tasks_url)
+    user_req = requests.get(user_url)
+    data_task = json.loads(tasks_req.text)
+    data_user = json.loads(user_req.text)
+    user = data_user[0].get('name')
+    number_of_task = len(data_task)
+    done_task = 0
+    completed_task = []
+    for task in data_task:
+        if task.get('completed'):
+            done_task += 1
+            completed_task.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user, done_task, number_of_task))
+    for title in completed_task:
+        print("\t{}".format(title))
