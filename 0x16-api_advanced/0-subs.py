55e7573e8b52b3f84c333f4c0d68232d0a47dd2b
jalondono <juanlondono151776@hotmail.com>
2020-02-06 13:16:52 -0500

    Upload task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,8 @@ def number_of_subscribers(subreddit):
     base_url = 'https://www.reddit.com/r/'
     url = base_url + subreddit + "/about.json"
     cred = {'User-Agent': 'testin'}
-    url2 = 'https://www.reddit.com/r/' + subreddit + "/about.json"
-    req = requests.get(url2, headers=cred, allow_redirects=False)
+    req = requests.get(url, headers=cred, allow_redirects=False)
     data = req.json()
-    print()
     if req.status_code != 200:
         return 0
     return req.json().get("data").get("subscribers")
