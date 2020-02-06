9f48e78309feb062864d9bd19d64cfc00f0b44f4
jalondono <juanlondono151776@hotmail.com>
2020-02-06 15:23:08 -0500

    Upload task 3

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""unction that queries the Reddit API
+ and returns the number of subscribers"""
+
+import json
+import requests
+from sys import argv
+
+
+def count_words(subreddit, word_list, after='', count={}):
+    word_list.sort()
+    base_url = 'https://www.reddit.com/r/'
+    url = base_url + subreddit + "/hot.json"
+    cred = {'User-Agent': 'testin'}
+    parameter = {'limit': '100', 'after': after}
+    req = requests.get(url, headers=cred, allow_redirects=False,
+                       params=parameter)
+    if req.status_code != 200:
+        return None
+    else:
+        request_sub = req.json()
+        after = (request_sub.get('data').get('after'))
+        for data in request_sub.get('data').get('children'):
+            for word in word_list:
+                if count.get(word) is None:
+                    count[word] = data.get('data').get('title').count(word) + 0
+                else:
+                    count[word] = data.get('data')\
+                                      .get('title').count(word) \
+                                  + count.get(word)
+    if after is None:
+        for key, value in count.items():
+            print("{}: {:d}".format(key, count[key]))
+        return count
+    return count_words(subreddit, word_list, after, count)
