76e6e844d843a2b4adc1ef9a860f80f732b6fee9
jalondono <juanlondono151776@hotmail.com>
2020-02-06 14:00:37 -0500

    Upload task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""unction that queries the Reddit API
+ and returns the number of subscribers"""
+
+import json
+import requests
+from sys import argv
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    base_url = 'https://www.reddit.com/r/'
+    url = base_url + subreddit + "/hot.json"
+    cred = {'User-Agent': 'testin'}
+    parameter = {'limit': '100', 'after': after}
+    req = requests.get(url, headers=cred, allow_redirects=False,
+                       params=parameter)
+    if req.status_code != 200:
+        print(None)
+    else:
+        request_sub = req.json()
+        after = (request_sub.get('data').get('after'))
+        for data in request_sub.get('data').get('children'):
+            hot_list.append(data)
+    if after is None:
+        return hot_list[:-1]
+    return recurse(subreddit, hot_list, after)
