d91bd127808a595143f9b03750ad5cc2f2a2040c
jalondono <juanlondono151776@hotmail.com>
2020-02-06 13:19:06 -0500

    Upload task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""unction that queries the Reddit API
+ and returns the number of subscribers"""
+
+import json
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    base_url = 'https://www.reddit.com/r/'
+    url = base_url + subreddit + "/hot.json"
+    cred = {'User-Agent': 'testin'}
+    parameter = {'limit': '10'}
+    req = requests.get(url, headers=cred, allow_redirects=False, params=parameter)
+    if req.status_code != 200:
+        return 0
+    request_sub = req.json()
+    for data in request_sub.get('data').get('children'):
+        print("{}".format(data.get('data').get('title')))
